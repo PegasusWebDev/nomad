9d4d5566962a0e088b68774bb45c7e725cfd3e9a
PegasusWebDev <121835328+PegasusWebDev@users.noreply.github.com>
2023-02-23 20:00:55 -0500

    test

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ navigator.serviceWorker.register('/offline.js', {scope: '.'});
 $(()=>{
 	screen.switch('menu');
 	const r = new Renderer($('canvas').get(0));
+	$('canvas').click((e)=>{
+		let x = Math.floor((e.offsetX/$('canvas').width())*105)
+		let y = Math.floor((e.offsetY/$('canvas').height())*105)
+		r.draw('./assets/game/blank_tile.png', x, y);
+	})
 	$('#menu-play').click(()=>{
 		screen.switch('game')
 		r.draw('./assets/game/blank_tile.png', 10, 10);

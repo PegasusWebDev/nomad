d024f226173d0524d67390b61afac8818b1ea541
PegasusWebDev <121835328+PegasusWebDev@users.noreply.github.com>
2023-02-26 19:16:11 -0500

    test

--- js/worldgen.mjs
+++ js/worldgen.mjs
@@ -19,7 +19,7 @@ export default class Level {
 		let i = c.createImageData(this.map.length,this.map[0].length), d = i.data;
 		for(let x = 0; x < this.map.length; x++){
 			for(let y = 0; y < this.map[x].length; y++){
-				let v = Math.abs(this.map[x][y] * 256);
+				let v = (this.map[x][y] + 1)*128;
 				let c = (x + y * this.map[x].length) * 4;
 				d[c] = d[c+1] = d[c+2] = v;
 				d[c+3] = 255;

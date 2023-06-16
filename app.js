731e3eddc2e6505e9f7b1c4973c70daa08ac8e0b
PegasusWebDev <121835328+PegasusWebDev@users.noreply.github.com>
2023-06-16 10:51:28 -0400

    i'm bad at this

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ navigator.serviceWorker.register('/offline.js', {scope: '.'});
 $(()=>{
 	screen.switch('menu');
 	const r = new Renderer($('canvas').get(0));
-	let level = new Level();
+	let level = new Level(r);
 	$('#travel').click((e)=>{
 		screen.switch('game', 1000);
 		setTimeout(()=>{
-			level = new Level();
+			level = new Level(r);
 		}, 1000);
 	})
 	$('#menu-play').click(()=>{

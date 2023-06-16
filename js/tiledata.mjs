fa8fd16f043c6c1fc07d8b915d590a1b4e2fcb5c
PegasusWebDev <121835328+PegasusWebDev@users.noreply.github.com>
2023-06-16 12:13:11 -0400

    neighbor fix

--- js/tiledata.mjs
+++ js/tiledata.mjs
@@ -16,6 +16,13 @@ export default {
         { left: "sand", right: "scarce_grass" },
         { left: "scarce_grass", right: "grass" },
         { left: "grass", right: "rocks" },
-        { left: "rocks", right: "peaks" }
+        { left: "rocks", right: "peaks" },
+        { left:"deep_water", right:"deep_water" },
+        { left:"grass", right:"grass" },
+        { left:"peaks", right:"peaks" },
+        { left:"rocks", right:"rocks" },
+        { left:"sand", right:"sand" },
+        { left:"scarce_grass", right:"scarce_grass" },
+        { left:"water", right:"water" },
     ]
 }

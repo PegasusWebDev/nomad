1198e0fa7bce2748154954907fb09cffea737557
PegasusWebDev <121835328+PegasusWebDev@users.noreply.github.com>
2023-06-19 13:35:16 -0400

    i'm awesome at this

--- js/lib/wavefunctioncollapse/simple-tiled-model.js
+++ js/lib/wavefunctioncollapse/simple-tiled-model.js
@@ -304,7 +304,7 @@ SimpleTiledModel.prototype.graphics = function (array, defaultColor) {
 SimpleTiledModel.prototype.graphicsComplete = function (array) {
   for (let x = 0; x < this.FMX; x++) {
     for (let y = 0; y < this.FMY; y++) {
-      const tile = randomRotate(this.tiles[this.observed[x + y * this.FMX]]);
+      const tile = randomRotate(this.tiles[this.observed[x + y * this.FMX]], this);
 
       for (let yt = 0; yt < this.tilesize; yt++) {
         for (let xt = 0; xt < this.tilesize; xt++) {
